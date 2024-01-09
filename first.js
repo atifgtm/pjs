aa2c7967b33a15bdd3072e95fe7579d56cadb892
asp <asp@aptech.com>
2024-01-09 20:34:09 +0500

    atif

---
+++ first.js
@@ -0,0 +1,43 @@
+console.log("welcome to pjs");
+console.log("hello world");
+
+// data types
+name ="mike";   // String
+age = 45;       // Int  , number
+iscinic = false;// boolean true $ false
+x = undefined;  //  Kch ana hai
+y = null;       //  empty
+console.table(name , age , iscinic , x , y);
+console.log(typeaf(name))
+console.log(typeaf(iscnic))
+console.log(typeaf(x))
+console.log(typeaf(age))
+
+
+// two types of dadta types
+
+// primitive string , int , number , boolean etc
+// non primitive array , funtions , objeat etc
+
+
+// three types of decleration
+// var can be redeclared and updated
+// let can,t be redeclared  but it can be updated
+// const can't be rredeclared or updated
+
+var city = "karachi";
+console.log(city);
+city = "lahore";
+console.log(city);
+var city ="peshawer";
+console.log(city);
+
+let myage =15;
+console.log(myage);
+myage = 80;
+console.log(myage);
+
+const pie = 3.14;
+console.log(pie);
+pie = 1040;
+conlsole.log(pie);
